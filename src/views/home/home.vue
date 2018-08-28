b55e02d5dec208a68c11ecc14441942ad5659f37
chen minhang <18758191423@163.com>
2018-08-28 16:19:41 +0800

    add home

---
+++ src/views/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container direction='vertical'>
+    <Header></Header>
+    <el-container>
+      <Aside v-if='false'></Aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+import Header from '../../components/header/header'
+import Aside from '../../components/aside/aside'
+
+export default {
+  components: {
+    Header,
+    Aside
+  }
+}
+</script>
+<style lang="less">
+</style>
