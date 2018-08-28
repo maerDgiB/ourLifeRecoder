1c379dbf478f7479324d21c5117044e75ae41857
chen minhang <18758191423@163.com>
2018-08-28 16:11:59 +0800

    add header

---
+++ src/components/header/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-header>
+    <span>便当流水记录</span>
+    <div class="left-wrapper">
+      <span class="user-name">{{userName}}</span>
+      <el-button class="quit">注销</el-button>
+    </div>
+  </el-header>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      userName: '林俊杰'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+@import './header.less';
+</style>
