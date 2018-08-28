a40e02872b676dd13cf04d64e83bde93a5bac621
chen minhang <18758191423@163.com>
2018-08-28 16:19:16 +0800

    [add] element-ui的css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './public/normalize.css'
 import ajaxUrl from './public/ajaxUrl'
 import _axios from 'axios'
 
