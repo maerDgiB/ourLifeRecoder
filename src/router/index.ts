1ffa20dbe390546f19b6b4817530def68b08864a
baixiaoji <1041874421@qq.com>
2018-09-10 08:36:01 +0800

    fix: fix ts file can't  find name 'require', please install @types/webpack-env

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router, { RouteConfig } from 'vue-router'
+// import Home from '../views/home/home.vue'
+
+Vue.use(Router)
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'login',
+    component: resolve => require(['../views/login/login.vue'], resolve),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: resolve => require(['../views/home/home.vue'], resolve),
+    // component: Home,
+    children: [
+      { path: '/', component: resolve => require(['../views/waterList/waterList.vue'], resolve) }
+    ]
+  }
+]
+
+export default new Router({
+  routes,
+})
