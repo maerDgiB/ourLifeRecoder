d8d72642c8b94a0c74282bf3a9aa3b7f5a5ac715
mario <18758191423@163.com>
2018-09-11 23:22:18 +0800

    [add] token

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import config from './config'
 
+axios.defaults.headers.common['Authorization'] = ''
 export default {
     login: body => axios.post(config.login, body),
     tableOrder: body => axios.get(config.tableOrder, body)
