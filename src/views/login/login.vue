25a8da8f30fc43552b7b9db6b701fdfa33b1ca76
Evilson <462806137@qq.com>
2018-09-11 23:06:34 +0800

    login页面简化

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,70 +1,63 @@
 <template>
   <div class="loginbox">
     <h1>登录</h1>
-    <el-form>
-      <el-form-item label="用户名" :model="ruleForm" :rules="rules">
-        <el-input type="text" id="user" v-model="formName.user" @blur="inputBlur('user',formName.user)"></el-input>
-        <p>{{formName.userError}}</p>
+    <el-form :model="loginForm" :rules="rules" ref="loginForm" status-icon>
+      <el-form-item label="用户名" prop='user'>
+        <el-input type="text" id="user" v-model="loginForm.user" autoFocus></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input type="password" id="password" v-model="formName.password" @blur="inputBlur('password',formName.password)"></el-input>
-        <p>{{formName.passwordError}}</p>
+      <el-form-item label="密码" prop='pwd'>
+        <el-input type="password" id="pwd" v-model="loginForm.pwd" ></el-input>
       </el-form-item>
-      <el-button class="upbtn" type="primary" @click="submitForm('formName')" v-bind:disabled="formName.beDisabled">提交</el-button>
-      <el-button @click="resetForm">重置</el-button>
+      <el-button type="primary" class="loginbtn" >登录</el-button>
     </el-form>
   </div>
 </template>
 <style lang="less" src="./login.less"></style>
 <script>
-export default {
-  data () {
-    return {
-      formName: { // 表单中的参数
-        user: '',
-        userError: '',
-        password: '',
-        passwordError: '',
-        beDisabled: true
-      },
-      beShow: false // 传值给父组件
-    }
-  },
-  methods: {
-    resetForm: function () {
-      this.formName.user = ''
-      this.formName.userError = ''
-      this.formName.password = ''
-      this.formName.passwordError = ''
-    },
-    submitForm: function (formName) {
-      var user = this.formName.user
-      var password = this.formName.password
-      console.log(user, password)
-      // ajax 是否登录成功
-    },
-    inputBlur: function (errorItem, inputContent) {
-      if (errorItem === 'user') {
-        if (inputContent === '') {
-          this.formName.userError = '用户名不能为空'
-        } else {
-          this.formName.userError = ''
-        }
-      } else if (errorItem === 'password') {
-        if (inputContent === '') {
-          this.formName.passwordError = '密码不能为空'
-        } else {
-          this.formName.passwordError = ''
+  export default {
+    data () {
+      var checkUser = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('用户名不能为空'))
         }
       }
-      // 对于按钮的状态进行修改
-      if (this.formName.user !== '' && this.formName.password !== '') {
-        this.formName.beDisabled = false
-      } else {
-        this.formName.beDisabled = true
+      var checkPwd = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('密码不能为空'))
+        }
+      }
+      return {
+        loginForm: {
+          user: '',
+          pwd: ''
+        },
+        rules: {
+          user: [
+            {validator: checkUser, trigger: 'blur'}
+          ],
+          pwd: [
+            {validator: checkPwd, trigger: 'blur'},
+            { min: 6, message: '密码长度最少为6位', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    mounted: function () {
+      this.postLogin()
+    },
+    methods: {
+      postLogin: function () {
+        let params = {
+          userName: '',
+          password: ''
+        }
+        this.$api.login(params).then((res) => {
+          if (res) {
+            console.log(res)
+          }
+        })
       }
     }
-  }
 }
 
 </script>
---
+++ src/views/waterList/waterList.js
@@ -0,0 +1,6 @@
+/*
+* @Author: Evilson
+* @Date:   2018-08-28 20:50:04
+* @Last Modified by:   Evilson
+* @Last Modified time: 2018-08-30 23:09:46
+*/
