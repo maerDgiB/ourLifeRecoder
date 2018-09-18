f687760108aeb039b286ef621098afcdbd5fb058
mario <18758191423@163.com>
2018-09-18 21:04:58 +0800

    [add] 订单修改和订单添加

--- src/api/config.js
+++ src/api/config.js
@@ -3,5 +3,6 @@ const base = 'http://192.168.0.103/api'
 export default {
   login: `${base}/Account/Token`,
   tableOrder: `${base}/Order`,
-  nickNames: `${base}/Account/NickNames`
+  nickNames: `${base}/Account/NickNames`,
+  addOrder: `${base}/Order`
 }
