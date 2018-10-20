3cae8568376a509954074058abf909c7460e00ba
Mario <18758191423@163.com>
2018-10-20 14:28:35 +0800

    [add] loginout function

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -3,7 +3,7 @@
     <span>便当流水记录</span>
     <div class="left-wrapper">
       <span class="user-name">{{userName}}</span>
-      <el-button class="quit">注销</el-button>
+      <el-button class="quit" @click="quit">注销</el-button>
     </div>
   </el-header>
 </template>
@@ -12,7 +12,28 @@
 export default {
   data () {
     return {
-      userName: '林俊杰'
+      userName: localStorage.getItem('userName')
+    }
+  },
+  methods: {
+    quit: function () {
+      this.$confirm('是否注销?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          localStorage.removeItem('token')
+          this.$message({
+            type: 'success',
+            message: '注销成功!'
+          })
+          this.$router.push({path: '/'})
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消注销'
+          })
+        })
     }
   }
 }
