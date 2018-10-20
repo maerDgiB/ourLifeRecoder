c0c767b71e2aaa9cf1c6358578cba2c85a727e94
Mario <18758191423@163.com>
2018-10-20 14:27:39 +0800

    [add] add api

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import config from './config'
 
-axios.defaults.headers.common['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2Yjg4Mzc4YS0zNWU0LTRkNzAtNDVhNS0wOGQ2MjFkYTYyMDkiLCJHcm91cCI6ImViZmJjZDhhLTU5NWQtNDU3Zi1iZDJhLWMyMDA3ZDI3ZTRjOCIsIlJvbGUiOiJDbGllbnQiLCJpYXQiOiIyMDE4LzkvMjYgMTQ6MjM6NDUiLCJleHAiOjE1Mzc5NzkwMjUsImlzcyI6IkxpZmVSZWNvZGVyIn0.v9iJ1yjqye9XpSX81dsKxc8AWGaeblvEVLF5jP7Bhx4'
 export default {
     login: body => axios.post(config.login, body),
     tableOrder: body => axios.get(config.tableOrder, body),
     nickNames: () => axios.get(config.nickNames),
     addOrder: body => axios.post(config.addOrder, body),
-    editOrder: body => axios.put(config.addOrder, body)
+    editOrder: body => axios.put(config.addOrder, body),
+    getTeam: body => axios.get(config.getTeam, body)
 }
--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,13 @@
-const base = 'http://api.bqggg.com/api'
-
+// const base = 'http://api.bqggg.com/api'
+import {Base64} from 'js-base64'
+const base = 'http://47.106.234.64/api'
+let token = localStorage.getItem('token')
+let toknInfo = token ? Base64.decode(token) : ''
+let groupId = token ? JSON.parse(toknInfo.split('}')[1] + '}').Group : ''
 export default {
   login: `${base}/Account/Token`,
   tableOrder: `${base}/Order`,
   nickNames: `${base}/Account/NickNames`,
-  addOrder: `${base}/Order`
+  addOrder: `${base}/Order`,
+  getTeam: `${base}/Team/${groupId}`
 }
